c59d8b28a10b85d3cb6b3ef87b13da0e1365eb0d
sadat <tauseefahmed782@gmail.com>
2026-02-14 15:14:49 +0530

    margin issue

--- components/home/PhoneLaunchSlider.js
+++ components/home/PhoneLaunchSlider.js
@@ -57,8 +57,8 @@ export default function PhoneLaunchSlider() {
 
                 {/* Slide 2 */}
                 <SwiperSlide>
-                    <div className="flex items-center justify-center h-full ">
-                        <div className="grid grid-cols-1 md:grid-cols-[35%_65%] items-center gap-8">
+                    <div className="flex  justify-center h-full ">
+                        <div className="grid grid-cols-1 md:grid-cols-[35%_65%]  gap-8">
                             
                             <div>
                               <div className="inline-flex  items-center bg-[##FBFBFB] rounded-full border border-[#D4D4D4A3] px-3 py-2">
@@ -74,7 +74,7 @@ export default function PhoneLaunchSlider() {
       </span>
 
     </div>
-                                <h1 className="text-black font-medium text-3xl md:text-4xl lg:text-7xl font-inter leading-tight mb-6">
+                                <h1 className="text-black mt-2 font-medium text-3xl md:text-4xl lg:text-7xl font-inter leading-tight mb-6">
               We have the MacBook you need.
             </h1>
 
